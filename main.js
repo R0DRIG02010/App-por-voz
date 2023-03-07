75960424bcd7c7a8d6f1297b7ef408c48b3080e5
R0DRIG02010 <108089716+R0DRIG02010@users.noreply.github.com>
2023-03-07 14:54:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ camera = document.getElementById("camera");Webcam.set({
 function  take_snapshot ()
 {
  Webcam.snap(function(data_url){
-    document.getElementById("result").innerHTML = '<img id="selfie_image" src+data_uri+"/';
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
 
  });
 
